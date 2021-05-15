c6b2ab29f920f0d8fe85ee6fbbaddc55b9a87ac8
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-05-15 19:40:34 +0530

    Added:count of head and tails

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-Flip=$(($(($RANDOM%10))%2))
-if [ $Flip -eq 1 ]
-then
-    echo "heads"
-else
-    echo "tails"
-fi
+headcount=0
+tailcount=0
+n=0
+while [ $n -le 20 ]
+do
+	 n=$((n+1))
+    Flip=$(($(($RANDOM%10))%2))
+    if [ $Flip -eq 1 ]
+    then
+        echo "heads"
+        headcount=$((headcount+1))
+    else
+        echo "tails"
+        tailcount=$((tailcount+1))
+    fi
+done
+echo "head count is" $headcount "and tail count is" $tailcount
+
